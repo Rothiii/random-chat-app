b94d613cbd946b23e972bb073e52dccea42a0b9b
Rothiii <drome.emord@gmail.com>
2024-10-09 20:21:46 +0800

    feat: slicing login and sign up

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -6,131 +6,154 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Background color matching the design
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // App Logo
-            // Image.asset(
-            //   'assets/logo.png', // Place your logo image in the assets folder and set this path
-            //   height: 100, // Adjust height as needed
-            // ),
-            const SizedBox(height: 40),
+        backgroundColor: Colors.white, // Background color matching the design
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // App Logo
+                Image.asset(
+                  'assets/images/image_loginscreen.png',
+                  height: 250, // Adjust height as needed
+                ),
+                const SizedBox(height: 40),
 
-            // User Login Text
-            const Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 0, 0, 0),
-              ),
-            ),
-            const SizedBox(height: 40),
+                // User Login Text
 
-            // Username Input Field
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: const Icon(
-                  Icons.person,
-                  color: Colors.white,
-                ),
-                hintText: 'Username',
-                hintStyle: const TextStyle(color: Colors.black),
-                filled: true,
-                fillColor: Colors.orange,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              style: const TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 20),
-
-            // Password Input Field
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(
-                  Icons.lock,
-                  color: Colors.white,
-                ),
-                hintText: 'Password',
-                hintStyle: const TextStyle(
-                  color: Colors.white54,
-                ),
-                filled: true,
-                fillColor: Colors.orange,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 30),
-
-            // Login Button
-            ElevatedButton(
-              onPressed: () {
-                // Handle login logic
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.orange,
-                backgroundColor: Colors.black, // Text color
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.symmetric(vertical: 16),
-              ),
-              child: const Center(
-                child: Text(
-                  'Sign Up',
+                const Text(
+                  'Welcome To Anonymous Chat',
                   style: TextStyle(
-                    fontSize: 18,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+
+                const SizedBox(height: 40),
+
+                // Username Input Field
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                    hintText: 'Username',
+                    hintStyle: const TextStyle(color: Colors.white54),
+                    filled: true,
+                    fillColor: Colors.orange,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+
+                // Password Input Field
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(
+                      Icons.lock,
+                      color: Colors.white,
+                    ),
+                    hintText: 'Password',
+                    hintStyle: const TextStyle(
+                      color: Colors.white54,
+                    ),
+                    filled: true,
+                    fillColor: Colors.orange,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 30),
+
+                // Login Button
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle login logic
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.orange,
+                    backgroundColor: Colors.black87, // Text color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // Sign Up Button
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontSize: 16,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Handle sign up navigation
+                        Navigator.pushNamed(context, '/SignUp');
+                      },
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  "Or",
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
-
-            // Sign Up Button
-            TextButton(
-              onPressed: () {
-                // Handle sign up navigation
-              },
-              child: const Text(
-                'Doesn\'t have an account? Sign Up',
-                style: TextStyle(
-                  color: Colors.orange,
-                  fontSize: 16,
+                // Login as Guest Button
+                TextButton(
+                  onPressed: () {
+                    // Handle login as guest
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text(
+                    'Login as Guest',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-            const Text(
-              "Or",
-              style: TextStyle(color: Colors.black),
-            ),
-            // Login as Guest Button
-            TextButton(
-              onPressed: () {
-                // Handle login as guest
-              },
-              child: const Text(
-                'Login as Guest',
-                style: TextStyle(
-                  color: Colors.orange,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
