4e4ce42fcdf16d13c69c8f824d65f78aabe3b9cd
Rothiii <11211068@student.itk.ac.id>
2024-11-13 12:19:05 +0800

    feat: implement api socket, 50% maybe

--- lib/chat_anonym_screen.dart
+++ lib/chat_anonym_screen.dart
@@ -1,6 +1,9 @@
+import 'package:anonymous_chat/api/api_endpoint.dart';
+import 'package:anonymous_chat/utils/shared_preferences_manager.dart';
 import 'package:anonymous_chat/widget/chat_bubble_anonym.dart';
 import 'package:flutter/material.dart';
 import 'package:anonymous_chat/service/socket_service.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ChatAnonymScreen extends StatefulWidget {
   const ChatAnonymScreen({super.key});
@@ -12,28 +15,64 @@ class ChatAnonymScreen extends StatefulWidget {
 class _ChatAnonymScreenState extends State<ChatAnonymScreen> {
   final TextEditingController _controller = TextEditingController();
   final ScrollController _scrollController = ScrollController();
-  final SocketService _socketService = SocketService();
+  SharedPreferencesManager prefManager = SharedPreferencesManager();
+  late IO.Socket socket;
+  String token = '';
   bool _isSearching = true;
   List<Map<String, dynamic>> _messages = [];
 
   @override
   void initState() {
     super.initState();
-    _socketService.connect();
+    initializeSocket();
+  }
 
-    _socketService.socket?.on('matching', (_) {
+  Future<void> getToken() async {
+    token = await prefManager.getToken();
+  }
+
+  Future<void> initializeSocket() async {
+    await getToken();
+    if (token.isNotEmpty) {
+      connectToServer();
+    }
+  }
+
+  void connectToServer() {
+    socket = IO.io(ApiEndPoints.chatAnonym, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': true,
+      'extraHeaders': {'authorization': 'Bearer $token'},
+    });
+    socket.on('connect', (_) {});
+    socket.on('disconnect', (_) {});
+    socket.on('matching', (_) {
+      print("Received matching event from server");
       setState(() {
         _isSearching = false;
       });
     });
-
-    _socketService.socket?.on('partner_left', (_) {
-      Navigator.pushReplacementNamed(context, '/Home');
+    socket.on('partnerDisconnect', (_) {
+      print("Partner disconnected");
+      setState(() {
+        _isSearching = true;
+      });
     });
-
-    _socketService.socket?.on('receiveMessage', (data) {
+    socket.on('receiveMessage', (data) {
       setState(() {
         _messages.add({'content': data['message'], 'isMe': false});
+      });
+      _scrollToBottom();
+    });
+    socket.on('joinRoom', (data) {
+      setState(() {
+        _isSearching = false;
+      });
+      socket.emit('joinRoomAck', data[0]);
+    });
+    socket.on('sendMessage', (data) {
+      setState(() {
+        _messages.add({'content': data['message'], 'isMe': true});
       });
       _scrollToBottom();
     });
@@ -41,7 +80,7 @@ class _ChatAnonymScreenState extends State<ChatAnonymScreen> {
 
   @override
   void dispose() {
-    _socketService.disconnect();
+    socket.disconnect();
     _controller.dispose();
     _scrollController.dispose();
     super.dispose();
@@ -63,8 +102,7 @@ class _ChatAnonymScreenState extends State<ChatAnonymScreen> {
       setState(() {
         _messages.add({'content': message, 'isMe': true});
       });
-      _socketService.sendMessage(message);
-      print('Sent: $message');
+      socket.emit('sendMessage', message);
       _controller.clear();
       _scrollToBottom();
     }
@@ -79,7 +117,8 @@ class _ChatAnonymScreenState extends State<ChatAnonymScreen> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
-            Navigator.pushNamed(context, '/Home');
+            Navigator.pushNamedAndRemoveUntil(
+                context, '/Home', (route) => false);
           },
         ),
         title: const Text(
@@ -96,53 +135,64 @@ class _ChatAnonymScreenState extends State<ChatAnonymScreen> {
           ),
         ],
       ),
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                controller: _scrollController,
-                padding: const EdgeInsets.all(16.0),
-                itemCount: _messages.length,
-                itemBuilder: (context, index) {
-                  final message = _messages[index];
-                  return ChatBubble(
-                    content: message['content'] ?? '',
-                    isMe: message['isMe'] ?? false,
-                  );
-                },
-              ),
-            ),
-            Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              color: Colors.black,
-              child: Row(
+      body: _isSearching
+          ? const Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(height: 16),
+                Text('Searching for a partner...',
+                    style: TextStyle(fontSize: 18)),
+              ],
+            ))
+          : GestureDetector(
+              onTap: () => FocusScope.of(context).unfocus(),
+              child: Column(
                 children: [
                   Expanded(
-                    child: TextField(
-                      controller: _controller,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText: "Type a message",
-                        hintStyle:
-                            TextStyle(color: Colors.white.withOpacity(0.5)),
-                        border: InputBorder.none,
-                      ),
-                      onSubmitted: (_) => _sendMessage(),
+                    child: ListView.builder(
+                      controller: _scrollController,
+                      padding: const EdgeInsets.all(16.0),
+                      itemCount: _messages.length,
+                      itemBuilder: (context, index) {
+                        final message = _messages[index];
+                        return ChatBubble(
+                          content: message['content'] ?? '',
+                          isMe: message['isMe'] ?? false,
+                        );
+                      },
                     ),
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.send, color: Colors.white),
-                    onPressed: _sendMessage,
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    color: Colors.black,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _controller,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: "Type a message",
+                              hintStyle: TextStyle(
+                                  color: Colors.white.withOpacity(0.5)),
+                              border: InputBorder.none,
+                            ),
+                            onSubmitted: (_) => _sendMessage(),
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.send, color: Colors.white),
+                          onPressed: _sendMessage,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
